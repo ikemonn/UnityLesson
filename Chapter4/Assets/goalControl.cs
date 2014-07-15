bfc97ee871cdbf57d35881ec3f324ebfd61f084f
daiki_ikemoto <ikemoto.daiki@gmail.com>
2014-07-15 23:14:14 +0900

    4章終了

---
+++ Chapter4/Assets/goalControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goalControl : MonoBehaviour {
+	private bool is_collided = false;
+	public float GOAL_MIN = 2.0f;
+	public float GOAL_MAX = 3.0f;
+	public float GOAL_MOVE = 0.5f;
+	private Vector3 goal_pos;
+	private Vector3 start_goal_pos;
+	private bool is_move_right = true;
+
+	// Use this for initialization
+	void Start () {
+		// GOAL_MIN ~ GOAL_MAXの間のランダムな値を取得
+		float rnd = Random.Range (GOAL_MIN, GOAL_MAX);
+		// GoalのX位置をランダムな値に
+		start_goal_pos = this.transform.position = new Vector3 (rnd, -1.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ゴールの位置
+		goal_pos = this.transform.position;
+		//左右どちらに進むかフラグ管理
+		if(goal_pos.x >= start_goal_pos.x + GOAL_MOVE){
+			is_move_right = false;                     // ゴールの可動領域の右端までいくと右へ進むフラグをfalseに
+		} else if (goal_pos.x <= start_goal_pos.x - GOAL_MOVE){
+			is_move_right = true;                     // ゴールの可動領域の左端までいくと右へ進むフラグをtrueに
+		}
+
+		// trueなら右に、falseなら左に進む
+		if (is_move_right == true) {
+			this.transform.Translate (Vector3.right * 1.0f * Time.deltaTime);
+		} else {
+			this.transform.Translate (Vector3.left * 1.0f * Time.deltaTime);
+		}
+
+
+
+	
+	}
+
+	// 他のGameObjectとぶつかっている間呼ばれ続ける
+	void OnCollisionStay(Collision other){
+		this.is_collided = true;
+	}
+
+	void OnGUI(){
+		if(is_collided){
+			//画面に成功と表示
+			GUI.Label (new Rect(Screen.width/2, 80, 100, 20), "成功！！");
+		}
+	}
+}
---
+++ Chapter4/Assets/playerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControl : MonoBehaviour {
+	private float power;
+	public float POWERPLUS = 250.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 左ボタンが押されている間
+		if(Input.GetMouseButton(0)){
+			power += POWERPLUS * Time.deltaTime;
+		}
+		// 左ボタンが離されたら
+		if(Input.GetMouseButtonUp(0)){
+			// ためた力をx,yに反映させて右上にジャンプ
+			this.rigidbody.AddForce (new Vector3(power, power, 0));
+			power = 0.0f;
+		}
+
+		// 地面より下に落ちたらシーンをリロード
+		if(this.transform.position.y < -5.0f){
+			Application.LoadLevel ("gameScene");
+		}
+	}
+}
